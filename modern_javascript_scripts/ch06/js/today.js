af85884de111b102f6f1464358f22cbcfc176884
FreeDaGeek <free.rosas@gmail.com>
2013-12-13 02:15:18 -0500

    Completed Today "Practice"

--- modern_javascript_scripts/ch06/js/today.js
+++ modern_javascript_scripts/ch06/js/today.js
@@ -1,29 +1,20 @@
-// today.js
-// This script indicates the current date and time.
+//demo
 
-// Call this function when the page has loaded:
-function init() {
-    
-    // Want to be strict:
+
+
+function init(){
     'use strict';
 
-    // Create a Date object:
-    var today = new Date();
+    var today = new Date(); //today var = Date method
+    var message = 'Right now it is' + today.toLocaleDateString(); // message var with today attached with a method to show the local date/time
+        message += ' at ' + today.getHours() + ':' + today.getMinutes(); //since the previous method displays the second we can ask for specifics with in the message var
+    var output = document.getElementById('output'); //created a var that selects the html element to later use to display local time and date
 
-    // Create a custom message:
-    var message = 'Right now it is ' + today.toLocaleDateString();
-    message += ' at ' + today.getHours() + ':' + today.getMinutes();
-    
-    // Get a reference to the paragraph:
-    var output = document.getElementById('output');
-    
-    // Update the innerText or textContent property of the paragraph:
-	if (output.textContent !== undefined) {
-		output.textContent = message;
-	} else {
-		output.innerText = message;
-	}
-    
-} // End of init() function.
+    if(output.textContent !==undefined){ //IF statement (if the output (with the text content method)is undefined show the message w/ that same text Content method
+        output.textContent = message;
+    }else{
+        output.innerText = message; // if not show the message w/ innerContent method
+    }
+}
 
-window.onload = init;
+init(); // calling the function above
