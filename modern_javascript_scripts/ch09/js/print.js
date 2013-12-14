7fdba484512c43e18b7d2f844e840f46f9687bae
FreeDaGeek <free.rosas@gmail.com>
2013-12-14 11:03:30 -0500

    Synatx fixed on the guessing game

--- modern_javascript_scripts/ch09/js/print.js
+++ modern_javascript_scripts/ch09/js/print.js
@@ -1,29 +1,18 @@
 // Script 9.7 - print.js
 
-// Establish functionality on window load:
-window.onload = function() {
-    'use strict';
+    window.onload = function(){
+        if (typeof window.print == 'function'){
+            var printButton = document.createElement('button'); //created a button
 
-    // Check for print support:
-    if (typeof window.print == 'function') {
-        
-        // Create a button:
-        var printButton = document.createElement('button');
-        
-        // Add the text:
-		if (printButton.textContent != 'undefined') {
-		    printButton.textContent = 'Print';
-		} else {
-		    printButton.innerText = 'Print';
-		}
-        
-        // Add a click handler:
-        printButton.onclick = function() {
-            window.print();
-        };
-        
-        // Add the button to the page:
-        document.body.insertBefore(printButton, document.getElementById('main'));
-        
-    } // End of IF.
-}; // End of onload anonymous function.
+          if(printButton.textContent != 'undefined'){
+              printButton.textContent = 'Print';
+          }else{
+              printButton.innerText = 'Print';
+          }
+        printButton.onclick = function(){
+                window.print();
+          };
+            document.body.insertBefore(printButton, document.getElementById('main'));
+        }// end if statement
+
+    };//end of onload function
